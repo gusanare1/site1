032365a1f30fff76aab96bde23974cd8e780ed12
gusanare1 <gang_emelec@yahoo.com>
2018-09-24 09:08:11 -0500

    no interesa
    No interesa

--- site1/urls.py
+++ site1/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
 from django.conf.urls.static import static
 from django.conf import settings
 
-#if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
